200064bac7f85e431a2c7a485e72ff2b35b60c0e
Izumo1982 <84167062+Izumo1982@users.noreply.github.com>
2021-07-16 20:05:36 +0900

    Add files via upload

---
+++ fizzbuzz_method.rb
@@ -0,0 +1,17 @@
+﻿class Fb
+  def fizzbuzz(num)
+	  str = 'Fizz'      if num % 3 == 0
+	  str = 'Buzz'      if num % 5 == 0
+	  str = 'Fizz Buzz' if num % 15 == 0
+	  str ||= num.to_s
+  end
+
+  def <<
+    for i in 1..100 do
+      p fizzbuzz(i)
+    end
+  end
+end
+
+fb = Fb.new
+fb.<<
